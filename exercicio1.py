956e01ceb380f3d4bd2e0d7f94ee8188c6de04f1
Cleber Prado <93355298+cleberprado@users.noreply.github.com>
2023-11-28 01:06:56 -0300

    exercicio de python par impar

---
+++ exercicio1.py
@@ -0,0 +1,13 @@
+numero = input('Digite um numero inteiro ')
+
+if numero.isdigit():
+    numero_int = int(numero)
+    par_impar = numero_int % 2 == 0
+    par_impar_texto = 'impar'
+
+
+    if par_impar:
+        par_impar_texto = 'par'
+        print(f'O numero {numero_int} é {par_impar_texto}')
+    else:
+        print('voce nao digitou um numero inteiro')
